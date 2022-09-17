13be947a21a13ad45ecd05b712d080e4a113ccc6
Akarsh Shrivastava <akarsh@metafic.co>
2022-09-18 00:47:40 +0530

    fixed the errors and now the update functions works in database

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Alert,
   Button,
@@ -11,79 +11,84 @@ import {
 } from 'react-native';
 import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, addDoc, getDocs, setDoc, doc, updateDoc } from "firebase/firestore";
-import 'dotenv/config';
 import TaskEditModal from './components/TaskEditModal';
 
 const App = () => {
-  const [tasks , setTasks] = useState<any>([])
-  const [input , setInput] = useState("")
+  const [tasks, setTasks] = useState<any>([])
+  const [input, setInput] = useState("")
   const [modalVisible, setModalVisible] = useState(false);
   const [taskIndex, setTaskIndex] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
 
-  
+
   const firebaseConfig = {
-    apiKey:  process.env.apiKey,
-    authDomain: process.env.authDomain,
-    projectId: process.env.projectId,
-    storageBucket: process.env.storageBucket,
-    messagingSenderId: process.env.messagingSenderId,
-    appId: process.env.appId,
-    measurementId: process.env.measurementId
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
-  
+
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   const db = getFirestore(app);
-  
+  const dbname = "users"
+
   useEffect(() => {
     getData()
   }, [])
-  
-  const getData = async ()=>{
-    let temp:string[] = []
-      const querySnapshot = await getDocs(collection(db, "users"));
-      querySnapshot.forEach((doc:any) => {
-        if(doc.data().task !== ("" || null || undefined))
-          temp.push(doc.data().task)
-       });  
-    
-      setTasks(temp) 
+
+  const getData = async () => {
+    let temp: object[] = []
+    const querySnapshot = await getDocs(collection(db, dbname));
+    querySnapshot.forEach((doc: any) => {
+      if (doc.data().task !== ("" || null || undefined))
+        temp.push({ task: doc.data().task, id: doc.id })
+    });
+
+    setTasks(temp)
+    setIsLoading(false)
   }
-  
-  const handleSubmit = async ()=>{
-    if(input !== ''){
-      setTasks([...tasks, input])
+
+  const handleSubmit = async () => {
+    if (input !== '') {
+      let tempID = Date.now().toString()
+      setTasks([...tasks, { task: input, id: tempID}])
       setInput("")
       try {
-        await setDoc(doc(db, "users", Date.now().toString()), {
+        await setDoc(doc(db, dbname, tempID), {
           task: input,
-          created_at: Date.now(),
+          created_at: tempID,
         });
-      
       } catch (e) {
         console.error("Error adding document: ", e);
       }
-      
     }
   }
 
-  const handleModalVisibility = (index:number) =>{
+  const handleModalVisibility = (index: number) => {
     setTaskIndex(index)
     setModalVisible(!modalVisible)
   }
 
-  const handleUpdatedTask = async (updatedTask: string, index: any)=>{
+  const handleUpdatedTask = async (updatedTask: string, index: any) => {
     setModalVisible(!modalVisible)
-    let temp = [...tasks]
-    temp[index] = updatedTask
-    setTasks(temp)
 
-    await updateDoc(doc(db, "users", "1663265913244"), {
+    let obj = tasks.find((o: any) => o.id == index);
+    const tempIndex = tasks.indexOf(obj);
+
+    let tempArray = [...tasks]
+    tempArray[tempIndex] = { task: updatedTask, id: index }
+    setTasks(tempArray)
+
+    await updateDoc(doc(db, dbname, index.toString()), {
       task: updatedTask,
     });
-    
+
   }
-  const handleDeleteTask = (index: any)=>{
+  const handleDeleteTask = (index: any) => {
     setModalVisible(!modalVisible)
     let temp = [...tasks]
     temp.splice(index, 1);
@@ -91,21 +96,21 @@ const App = () => {
   }
 
   return (
-  <View style={styles.rootView} >
-    <Text style={styles.topHeading} >  Enter a task </Text>
-    <TextInput
+    <View style={styles.rootView} >
+      <Text style={styles.topHeading} >  Enter a task </Text>
+      <TextInput
         style={styles.input}
         placeholder={'Enter your task here'}
-        onChangeText={(input)=>{
+        onChangeText={(input) => {
           setInput(input);
         }}
         value={input}
       />
-      <View style={styles.button }>
+      <View style={styles.button}>
         <Button
           title="Submit"
           onPress={handleSubmit}
-          color={'white'}
+          color='white'
           disabled={input.trim().length == 0}
         />
       </View>
@@ -117,26 +122,27 @@ const App = () => {
         handleDeleteTask={handleDeleteTask}
       />
 
-      {tasks.length !== 0 ? <FlatList
+      { isLoading ? 
+      <Text>Loading...</Text> : tasks.length !== 0 ? 
+      <FlatList
         data={tasks}
-        renderItem={(task)=>{
+        renderItem={(task) => {
           return (
             <View style={styles.tasksView}>
-              <Text onPress={()=>{handleModalVisibility(Number(task.index))}} >{task.item}</Text>
-              {/* <Text > Edit </Text> */}
+              <Text onPress={() => { handleModalVisibility(Number(task.item.id)) }} >{task.item.task}</Text>
             </View>
           )
         }}
       />
-    :
-    <Text style={styles.tasksView}>No Tasks available</Text>
-    }
-  </View>
+      :
+      <Text style={styles.tasksView}>No Tasks available</Text>
+      }
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  
+
   rootView: {
     marginTop: 60,
   },
@@ -146,7 +152,7 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginLeft: 8
   },
-  input:{
+  input: {
     height: 48,
     margin: 16,
     borderWidth: 1,
@@ -159,10 +165,10 @@ const styles = StyleSheet.create({
     margin: 16,
     padding: 4,
     borderRadius: 8,
-    backgroundColor:'#0193E0',
+    backgroundColor: '#0193E0',
   },
-  tasksView:{
-    flexDirection:'row',
+  tasksView: {
+    flexDirection: 'row',
     justifyContent: 'space-between',
     marginHorizontal: 16
   },
